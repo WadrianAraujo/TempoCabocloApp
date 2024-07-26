8dff78ee242663b6bff15dd84221d6bc4b5df0db
Wadrian Araujo De Moura <wadrian.araujo@gmail.com>
2024-07-25 21:04:05 -0400

    feat: manual location search
    layout and navigation

--- app/src/main/java/com/wax/tempocabocloapp/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/wax/tempocabocloapp/ui/fragments/home/HomeFragment.kt
@@ -12,7 +12,9 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.LocationServices
+import com.wax.tempocabocloapp.R
 import com.wax.tempocabocloapp.adapters.WeatherDataAdapter
 import com.wax.tempocabocloapp.data.CurrentLocation
 import com.wax.tempocabocloapp.databinding.FragmentHomeBinding
@@ -39,7 +41,7 @@ class HomeFragment : Fragment() {
         }
     )
 
-    private val sharedPreferencesManager : SharedPreferencesManager by inject()
+    private val sharedPreferencesManager: SharedPreferencesManager by inject()
 
     private val locationPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -64,7 +66,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setWeatherDataAdapter()
-        setWeatherData(currentLocation = sharedPreferencesManager.getCurrentLocation()))
+        setWeatherData(currentLocation = sharedPreferencesManager.getCurrentLocation())
         setObservers()
     }
 
@@ -125,6 +127,7 @@ class HomeFragment : Fragment() {
             setItems(options) { _, which ->
                 when (which) {
                     0 -> proceedWithCurrentLocation()
+                    1 -> startManualLocationSearch()
                 }
             }
             show()
@@ -144,6 +147,10 @@ class HomeFragment : Fragment() {
             weatherDataRecyclerView.visibility = View.VISIBLE
             swipeRefreshLayout.isRefreshing = false
         }
+    }
+
+    private fun startManualLocationSearch(){
+        findNavController().navigate(R.id.action_homeFragment_to_locationFragment)
     }
 
 }
