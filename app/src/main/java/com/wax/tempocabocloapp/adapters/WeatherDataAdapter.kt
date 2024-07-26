ef9e33984745ce82eaccb02f79939f054bd59eec
Wadrian Araujo De Moura <wadrian.araujo@gmail.com>
2024-07-26 14:26:55 -0400

    feat: diplay weather data

--- app/src/main/java/com/wax/tempocabocloapp/adapters/WeatherDataAdapter.kt
+++ app/src/main/java/com/wax/tempocabocloapp/adapters/WeatherDataAdapter.kt
@@ -3,13 +3,16 @@ package com.wax.tempocabocloapp.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.wax.tempocabocloapp.data.CurrentLocation
+import com.wax.tempocabocloapp.data.CurrentWeather
 import com.wax.tempocabocloapp.data.WeatherData
 import com.wax.tempocabocloapp.databinding.ItemContainerCurrentLocationBinding
+import com.wax.tempocabocloapp.databinding.ItemContainerCurrentWeatherBinding
 
 class WeatherDataAdapter(
     private val onLocationClicked: () -> Unit
-) : RecyclerView.Adapter<WeatherDataAdapter.CurrentLocationViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private companion object {
         const val INDEX_CURRENT_LOCATION = 0
@@ -20,12 +23,22 @@ class WeatherDataAdapter(
     private val weatherData = mutableListOf<WeatherData>()
 
     fun setCurrentLocation(currentLocation: CurrentLocation) {
-        if (weatherData.isEmpty()){
+        if (weatherData.isEmpty()) {
             weatherData.add(INDEX_CURRENT_LOCATION, currentLocation)
             notifyItemInserted(INDEX_CURRENT_LOCATION)
-        }else{
+        } else {
             weatherData[INDEX_CURRENT_LOCATION] = currentLocation
             notifyItemChanged(INDEX_CURRENT_LOCATION)
+        }
+    }
+
+    fun setCurrentWeather(currentWeather: CurrentWeather) {
+        if (weatherData.getOrNull(INDEX_CURRENT_WEATHER) != null) {
+            weatherData[INDEX_CURRENT_WEATHER] = currentWeather
+            notifyItemChanged(INDEX_CURRENT_WEATHER)
+        } else {
+            weatherData.add(INDEX_CURRENT_WEATHER, currentWeather)
+            notifyItemInserted(INDEX_CURRENT_WEATHER)
         }
     }
 
@@ -44,22 +57,57 @@ class WeatherDataAdapter(
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrentLocationViewHolder {
-        return CurrentLocationViewHolder(
-            ItemContainerCurrentLocationBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
+    inner class CurrentWeatherViewHolder(
+        private val binding: ItemContainerCurrentWeatherBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentWeather: CurrentWeather) {
+            with(binding) {
+                imageIcon.load("https:${currentWeather.icon}") { crossfade(true) }
+                textTemperature.text = String.format("%s\u00B0C", currentWeather.temperature)
+                textWind.text = String.format("%s km/h", currentWeather.wind)
+                textHumidity.text = String.format("%s%%", currentWeather.humidity)
+                textChanceOfRain.text = String.format("%s%%", currentWeather.chanceOfRain)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            INDEX_CURRENT_LOCATION -> CurrentLocationViewHolder(
+                ItemContainerCurrentLocationBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
             )
-        )
+
+            else -> CurrentWeatherViewHolder(
+                ItemContainerCurrentWeatherBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
     }
 
     override fun getItemCount(): Int {
         return weatherData.size
     }
 
-    override fun onBindViewHolder(holder: CurrentLocationViewHolder, position: Int) {
-        holder.bind(weatherData[position] as CurrentLocation)
+    override fun getItemViewType(position: Int): Int {
+        return when (weatherData[position]) {
+            is CurrentLocation -> INDEX_CURRENT_LOCATION
+            is CurrentWeather -> INDEX_CURRENT_WEATHER
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is CurrentLocationViewHolder -> holder.bind(weatherData[position] as CurrentLocation)
+            is CurrentWeatherViewHolder -> holder.bind(weatherData[position] as CurrentWeather)
+        }
+
     }
 
 }
